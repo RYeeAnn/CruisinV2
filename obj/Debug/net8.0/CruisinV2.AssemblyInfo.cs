c35ecdb1e4b2a59a27c1062656134343ec005886
Ryan Yee <ryeean16@gmail.com>
2024-12-08 04:31:43 -0800

    styling update for homepage

--- obj/Debug/net8.0/CruisinV2.AssemblyInfo.cs
+++ obj/Debug/net8.0/CruisinV2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CruisinV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d6475746214483c952a95d4ff796d24a97a108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00c84f988b75192757cb219987231b823ac62a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CruisinV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CruisinV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
